18f652f0fb9f6fd289c1b7f04068d8aa55e5b396
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-06-10 09:52:12 +0000

    update

--- src/routes/finance/wrapper.tsx
+++ src/routes/finance/wrapper.tsx
@@ -13,7 +13,7 @@ export const FinancePageWrapper: React.FC<React.PropsWithChildren> = ({
   return (
     <div className="page-container">
       {children}
-      <iframe title="Sinovuyo Gcanga" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1cdbedc3-1a55-422f-b28c-a105922ae725&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716" frameBorder="0" ></iframe>
+      <iframe title="Sinovuyo Gcanga" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1cdbedc3-1a55-422f-b28c-a105922ae725&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716&navContentPaneEnabled=false&filterPaneEnabled=false" frameBorder="0"></iframe>
     </div>
   );
 };
--- src/routes/graduate/wrapper.tsx
+++ src/routes/graduate/wrapper.tsx
@@ -13,7 +13,7 @@ export const GraduatePageWrapper: React.FC<React.PropsWithChildren> = ({
   return (
     <div className="page-container">
       {children}
-      <iframe title="Graduates2" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=37253408-fcb8-4b5f-8b4a-b5d14b93672e&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716" frameBorder="0" ></iframe>
+      <iframe title="Graduates2" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=37253408-fcb8-4b5f-8b4a-b5d14b93672e&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716&navContentPaneEnabled=false&filterPaneEnabled=false" frameBorder="0"></iframe>
     </div>
   );
 };
