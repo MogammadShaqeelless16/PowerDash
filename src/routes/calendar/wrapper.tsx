ce9c570aa515284618e4a6ea4307ccde3f39ce85
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-06-10 09:53:39 +0000

    update navs

--- src/routes/calendar/wrapper.tsx
+++ src/routes/calendar/wrapper.tsx
@@ -13,7 +13,7 @@ export const CalendarPageWrapper: React.FC<React.PropsWithChildren> = ({
   return (
     <div className="page-container">
       {children}
-      <iframe title="Enrollment" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ec189112-9c0e-47e6-9b0d-34d33a5daae5&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716" frameBorder="0"></iframe>
+      <iframe title="Enrollment" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=ec189112-9c0e-47e6-9b0d-34d33a5daae5&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716&navContentPaneEnabled=false&filterPaneEnabled=false" frameBorder="0"></iframe>
     </div>
   );
 };
--- src/routes/staff/wrapper.tsx
+++ src/routes/staff/wrapper.tsx
@@ -13,7 +13,7 @@ export const StaffPageWrapper: React.FC<React.PropsWithChildren> = ({
   return (
     <div className="page-container">
       {children}
-      <iframe title="KopanoMaluleke" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=384f22d8-98d2-4b96-b0df-5c485def6bfb&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716" frameBorder="0" ></iframe>
+      <iframe title="KopanoMaluleke" width="100%" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=384f22d8-98d2-4b96-b0df-5c485def6bfb&autoAuth=true&ctid=a3f14f21-237f-4028-b978-425eb768a716&navContentPaneEnabled=false&filterPaneEnabled=false" frameBorder="0"></iframe>
     </div>
   );
 };
